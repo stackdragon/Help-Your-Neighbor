02412b9c4659ef81c5b8af78bb75353fdae32131
Steve Perreault <perreaultstevej@gmail.com>
2020-04-21 13:37:29 -0400

    first commit

---
+++ project.py
@@ -0,0 +1,54 @@
+# import mysql connector module
+import mysql.connector
+
+#import modules for Flask, rendering templates, message display, and url generation
+from flask import Flask, render_template, url_for, flash, redirect
+
+#wtForm classes for registration and login forms
+from forms import RegistrationForm, LoginForm
+
+app = Flask(__name__)
+
+# this is a CSRF token for security -->
+app.config['SECRET_KEY'] = 'ba8ed3f480b55b599b397e000da63ccf'
+
+#home page route
+@app.route('/')
+@app.route('/home')
+def home():
+		return render_template('home.html')
+
+#about page route
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+#registration page route
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+
+    	# display success message if user successfully registered
+    	flash(f'Account created for {form.username.data}!', 'success')
+    	return redirect(url_for('home'))
+    return render_template('register.html', title='Register', form = form)
+
+# login page route
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+
+    	# display success message if user successfully logs in
+    	if form.email.data == 'test' and form.password.data == 'password':
+    		flash('You are now logged in!', 'success')
+    		return redirect(url_for('home'))
+    	else:
+    		flash('Username and password combination not found. Please try again.', 'danger')
+    return render_template('login.html', title='Login', form = form)
+
+
+# app will run in debug mode if run from terminal
+if __name__ == '__main__':
+    app.run(debug=True)
