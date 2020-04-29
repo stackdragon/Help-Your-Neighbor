5e5e99356022472df70131af7dbd557a7cad5b09
Steve Perreault <perreaultstevej@gmail.com>
2020-04-28 23:26:06 -0400

    setup fix-9

--- forms.py
+++ forms.py
@@ -1,10 +1,12 @@
+# this contains the classes for each of our main forms using wtforms
+
 from flask_wtf import FlaskForm
 from datetime import date, datetime
 from wtforms import StringField, PasswordField, SubmitField, BooleanField, SelectField
 from wtforms.fields.html5 import DateField, DateTimeField
-from wtforms.validators import DataRequired, Length, Email, EqualTo
+from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
 from wtforms_components import DateRange
-
+import mysql.connector
 
 class RegistrationForm(FlaskForm):
 	username = StringField('Username', validators=[DataRequired(), Length(min=2, max=20)])
@@ -12,6 +14,36 @@ class RegistrationForm(FlaskForm):
 	password = PasswordField('Password', validators=[DataRequired()])
 	confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
 	submit = SubmitField('Sign Up')
+
+	# custom validation to make sure that the username and password do not already exit in the Users table
+	def validate_username(self, username):
+
+		# connect to the database
+		db = mysql.connector.connect(host='us-cdbr-iron-east-01.cleardb.net', user='b94531a8a9be0d', password='440412d5', db='heroku_c22f6c727a9c888')
+		mycursor = db.cursor()
+
+		# run query to see if the username is already in the table
+		query = f"SELECT userName from Users WHERE userName='{username.data}';"
+		mycursor.execute(query)
+		user = mycursor.fetchall()
+
+		# display the validation error
+		if user:
+			raise ValidationError('Username already exists. Please choose a unique username.')
+			db.close()
+
+	def validate_email(self, email):
+		db = mysql.connector.connect(host='us-cdbr-iron-east-01.cleardb.net', user='b94531a8a9be0d', password='440412d5', db='heroku_c22f6c727a9c888')
+		mycursor = db.cursor()
+		
+		query = f"SELECT userEmail from Users WHERE userEmail='{email.data}';"
+		mycursor.execute(query)
+		user = mycursor.fetchall()
+
+		if user:
+			raise ValidationError('Email address already exists in our system. Please enter a unique email address.')
+
+		db.close()
 
 class LoginForm(FlaskForm):
 	email = StringField('Email', validators=[DataRequired(), Email()])
