c0148434627ca0af972445974a323d5c12c4aea8
Steve Perreault <perreaultstevej@gmail.com>
2020-04-28 21:19:33 -0400

    setup

---
+++ db_credentials.py
@@ -0,0 +1,5 @@
+
+host = 'classmysql.engr.oregonstate.edu' 
+user = 'cs340_perreaus 
+passwd = '8840' 
+db = 'cs340_perreaus' 
--- project.py
+++ project.py
@@ -1,13 +1,20 @@
 # import mysql connector module
 import mysql.connector
 
-#import modules for Flask, rendering templates, message display, and url generation
+# import bcrypt for password hashing
+from flask_bcrypt import Bcrypt
+
+# import modules for Flask, rendering templates, message display, and url generation
 from flask import Flask, render_template, url_for, flash, redirect
 
-#wtForm classes for registration and login forms
+# import wtForm classes for registration and login forms
 from forms import RegistrationForm, LoginForm, AddForm
 
+# name of webapp
 app = Flask(__name__)
+
+# create bcrpyt class object for password hashing
+bcrypt = Bcrypt(app)
 
 # this is a CSRF token for security -->
 app.config['SECRET_KEY'] = 'ba8ed3f480b55b599b397e000da63ccf'
@@ -16,7 +23,24 @@ app.config['SECRET_KEY'] = 'ba8ed3f480b55b599b397e000da63ccf'
 @app.route('/')
 @app.route('/home')
 def home():
-		return render_template('home.html')
+
+    # connect to the database
+    db = mysql.connector.connect(host='us-cdbr-iron-east-01.cleardb.net', user='b94531a8a9be0d', password='440412d5', db='heroku_c22f6c727a9c888')
+    
+    # set up db cursor
+    mycursor = db.cursor()
+
+    mycursor.execute("""DROP TABLE IF EXISTS diagnostic;""")
+    mycursor.execute("""CREATE TABLE diagnostic(id INT PRIMARY KEY, text VARCHAR(255) NOT NULL);""")
+    mycursor.execute("""INSERT INTO diagnostic (text) VALUES ("MySQL is working");""")
+    mycursor.execute("""SELECT * FROM diagnostic;""")
+
+    data = mycursor.fetchall()
+
+    # close cursor
+    db.close()
+	
+    return render_template('home.html', data = data)
 
 #about page route
 @app.route('/about')
@@ -29,9 +53,32 @@ def register():
     form = RegistrationForm()
     if form.validate_on_submit():
 
+        # hash the password that the user ended
+        hashed_pw = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
+
+        # connect to the database
+        db = mysql.connector.connect(host='us-cdbr-iron-east-01.cleardb.net', user='b94531a8a9be0d', password='440412d5', db='heroku_c22f6c727a9c888')
+
+        # set up db cursor
+        mycursor = db.cursor()
+
+        # run the query to add the user to the database
+        query = f"INSERT INTO Users (userID, userEmail, userPW) VALUES ('{form.username.data}', '{form.email.data}', '{hashed_pw}');"
+        mycursor.execute(query)
+
+        # commit the query
+        db.commit()
+
+        #close the database
+        db.close()
+
     	# display success message if user successfully registered
-    	flash(f'Account created for {form.username.data}!', 'success')
-    	return redirect(url_for('home'))
+        flash(f'Your account has been created. Please login.', 'success')
+
+        # render homepage
+        return redirect(url_for('home'))
+
+    # if no validation, render register page
     return render_template('register.html', title='Register', form = form)
 
 #add page route
@@ -60,7 +107,6 @@ def login():
     	else:
     		flash('Username and password combination not found. Please try again.', 'danger')
     return render_template('login.html', title='Login', form = form)
-
 
 # app will run in debug mode if run from terminal
 if __name__ == '__main__':
